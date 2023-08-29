dad1c45170d5803e375ac448cf0a6e3b85f17210
Mani Poorna <ndmprao2001@gmail.com>
2023-08-29 16:00:48 +0530

    V 1.0

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,13 @@
 import React from "react";
 import './../styles/App.css';
 
-const App = () => {
+const App = (props) => {
   return (
     <div>
         {/* Do not remove the main div */}
+        <h1>Welcome to my website</h1>
+        <p>This is the content of my website.</p>
+        <p>&copy; 2021 My Website. All rights reserved.</p>
     </div>
   )
 }
